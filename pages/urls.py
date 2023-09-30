fef4e82637b097a8eb43cfef29f33ec6bfb9b791
JosephRana11 <josephranamgr7@gmail.com>
2023-09-30 16:45:14 +0545

    contact updated

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home_view , login_view , logout_view , about_view
+from .views import home_view , login_view , logout_view , about_view , contact_view
 
 urlpatterns = [
   path('' , home_view , name = 'home'),
   path('login' , login_view , name='login'),
   path('logut' , logout_view , name='logout'),
   path('about' , about_view , name='about'),
+  path('contact' , contact_view , name='contact'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -30,3 +30,6 @@ def logout_view(request):
 
 def about_view(request):
   return render(request , 'about.html')
+
+def contact_view(request):
+  return render(request , 'contact.html')
